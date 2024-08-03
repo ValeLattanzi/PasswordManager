4aa455b610f92b0b201835bed7b8552585aeca02
ValeLattanzi <vale.lattanzi15@gmail.com>
2024-08-03 16:13:58 -0300

    publish

--- obj/Release/net8.0/PasswordManager.AssemblyInfo.cs
+++ obj/Release/net8.0/PasswordManager.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("PasswordManager")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+04995d71649e48ddce0d3bcbbf4d3221fe9aed9b")]
 [assembly: System.Reflection.AssemblyProductAttribute("PasswordManager")]
