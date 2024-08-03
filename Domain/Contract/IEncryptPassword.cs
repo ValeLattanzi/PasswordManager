8beae706a4edccb6a8f45d4529b1d79bc93925ea
ValeLattanzi <vale.lattanzi15@gmail.com>
2024-08-02 23:12:35 -0300

    init

---
+++ Domain/Contract/IEncryptPassword.cs
@@ -0,0 +1,7 @@
+﻿namespace PasswordManager.Domain.Contract;
+
+public interface IEncryptPassword
+{
+    public string Encrypt(string password);
+    bool VerifyPassword(string password, string hashedPassword);
+}
---
+++ Domain/UseCase/EncryptPassword.cs
@@ -0,0 +1,20 @@
+﻿using PasswordManager.Domain.Contract;
+
+namespace PasswordManager.Domain.UseCase;
+
+public class EncryptPassword : IEncryptPassword
+{
+    public EncryptPassword()
+    {
+    }
+
+    public string Encrypt(string password)
+    {
+        return BCrypt.Net.BCrypt.HashPassword(password);
+    }
+
+    public bool VerifyPassword(string password, string hashedPassword)
+    {
+        return BCrypt.Net.BCrypt.Verify(password, hashedPassword);
+    }
+}
